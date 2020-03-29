6742dca17b51232f1abfec53525cabe5859c1753
RYLiang18 <richard.y.liang@gmail.com>
2020-03-28 23:53:21 -0700

    aws, ubuntu stuff

---
+++ gst_frontend/build/precache-manifest.c9b78a9d90d5576e865f4532519ecd62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b1419ced4d5257e4353a89556e7b1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef316f44c9a14c3c552",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2900bd5afa19be2b8532",
+    "url": "/static/css/main.9551d90a.chunk.css"
+  },
+  {
+    "revision": "9ef316f44c9a14c3c552",
+    "url": "/static/js/2.24aa870f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.24aa870f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2900bd5afa19be2b8532",
+    "url": "/static/js/main.06c88016.chunk.js"
+  },
+  {
+    "revision": "1a66d2dbbe6ed4e1ee73",
+    "url": "/static/js/runtime-main.ef5a2bc1.js"
+  }
+]);
