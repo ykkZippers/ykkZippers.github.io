a339d2698d216926e0928229d7e1aa9b6416f817
ykkZippers <greed.h.ling@gmail.com>
2020-03-29 14:36:52 -0700

    Updates

---
+++ precache-manifest.b60167f47fa1ea435235584dce4520bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1025e22f2d9ee55a112b1ff12824fa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef316f44c9a14c3c552",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b40c7a6b7be965837033",
+    "url": "/static/css/main.9551d90a.chunk.css"
+  },
+  {
+    "revision": "9ef316f44c9a14c3c552",
+    "url": "/static/js/2.24aa870f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.24aa870f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40c7a6b7be965837033",
+    "url": "/static/js/main.9ca5f428.chunk.js"
+  },
+  {
+    "revision": "1a66d2dbbe6ed4e1ee73",
+    "url": "/static/js/runtime-main.ef5a2bc1.js"
+  }
+]);
