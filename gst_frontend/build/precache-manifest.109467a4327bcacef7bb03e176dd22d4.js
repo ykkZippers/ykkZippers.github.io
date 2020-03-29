adbac5180f576c255d4eb6f9a314db8646468c99
ykkZippers <greed.h.ling@gmail.com>
2020-03-29 14:33:15 -0700

    pl0x

---
+++ gst_frontend/build/precache-manifest.109467a4327bcacef7bb03e176dd22d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551aab3fcf2fb5458506035594b140fe",
+    "url": "/the_gst/index.html"
+  },
+  {
+    "revision": "9ef316f44c9a14c3c552",
+    "url": "/the_gst/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e95b1e700dff120042a6",
+    "url": "/the_gst/static/css/main.9551d90a.chunk.css"
+  },
+  {
+    "revision": "9ef316f44c9a14c3c552",
+    "url": "/the_gst/static/js/2.24aa870f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/the_gst/static/js/2.24aa870f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95b1e700dff120042a6",
+    "url": "/the_gst/static/js/main.cd214c3b.chunk.js"
+  },
+  {
+    "revision": "335e7d60c6dafa74917e",
+    "url": "/the_gst/static/js/runtime-main.7a9b75f5.js"
+  }
+]);
